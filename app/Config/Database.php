db7afaef7625b2cb1a3700a143764d3c3e0f326a
ChrissandraPetrisia12210780 <12210780@bsi.ac.id>
2022-09-19 17:09:23 +0700

    commit ke 4

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,7 +33,7 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => 'root',
+        'username' => 'soju',
         'password' => '',
         'database' => 'sekolah',
         'DBDriver' => 'MySQLi',
