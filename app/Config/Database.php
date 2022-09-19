bad2e94236be1e348693b7da727842a255facd33
feni <12210590@bsi.ac.id>
2022-09-19 17:39:35 +0700

    commit ke 5

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,7 +33,7 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => 'soju',
+        'username' => 'feni',
         'password' => '',
         'database' => 'sekolah',
         'DBDriver' => 'MySQLi',
