99994364c89dad8e50713e7712dc31227b6fb851
alfinaagustini12210692 <12210692@bsi.ac.id>
2022-09-19 16:51:16 +0700

    commit ke 3

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'db_sekolah',
+        'database' => 'sekolah',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
